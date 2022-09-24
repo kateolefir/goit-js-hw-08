7722b2fc61f49f33b74de49ce4188c4ad1699fea
kateolefir <kateryna.olefir@gmail.com>
2022-09-25 00:54:56 +0300

    fix submitForm

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,12 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = "feedback-form-state";
+
 const formData = {};
 const feedbackForm = document.querySelector('.feedback-form');
 
-// console.log(feedbackForm.elements.name('email').value);
 function onLoadDataForm() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
   if (!data) {
     return null;
   }
@@ -19,15 +20,14 @@ const changeFormInput = function (evt) {
   formData.email = feedbackForm.email.value;
   formData.message = feedbackForm.message.value;
 
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
 
 const submitForm = function (evt) {
   evt.preventDefault();
-
-  console.log(formData);
-  localStorage.removeItem('feedback-form-state');
-  feedbackForm.reset();
+  evt.currentTarget.reset();
+  console.log(localStorage.getItem(STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
 
   formData.email = '';
   formData.message = '';
